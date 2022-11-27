e831ae5d64f9a10ded35988fc8a108032e736382
Harish Krishnan <harishkrishnan1993@gmail.com>
2022-11-27 21:52:55 +0530

    Create Celeritas Repo along with myapp repo

---
+++ myapp/go.mod
@@ -0,0 +1,7 @@
+module myapp
+
+go 1.19
+
+replace github.com/harishkrishnan24/celeritas => ../celeritas
+
+require github.com/harishkrishnan24/celeritas v0.0.0-00010101000000-000000000000 // indirect
---
+++ myapp/init-celeritas.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/harishkrishnan24/celeritas"
+)
+
+func initApplication() *application {
+	path, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cel := &celeritas.Celeritas{}
+	err = cel.New(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cel.AppName = "myapp"
+	cel.Debug = true
+
+	app := &application{
+		App: cel,
+	}
+
+	return app
+}
